9220a0bec4d675d7c948919d181b1e8521d4ab48
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-11-13 17:27:55 +0200

    create VitalSignsCard and BabyInfoCard

--- components/VitalSignsCard.js
+++ components/VitalSignsCard.js
@@ -1,20 +1,54 @@
-import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
-import Card from "./Card";
+import React from 'react';
+import {Text, View, StyleSheet, Image} from 'react-native';
+import Card from './Card';
+
+function ColorRange(from, to, current) {
+  if (current >= from && to >= current) {
+    console.log('normal');
+    return '#C3F584';
+  } else if (current < from) {
+    console.log('low');
+    return '#FCCA4A';
+  } else if (current > to) {
+    console.log('high');
+    return '#FF3D00';
+  } else {
+    return '#6DE15A';
+  }
+}
+function Status(from, to, current) {
+  if (current >= from && to >= current) {
+    console.log('normal');
+    return 'Normal';
+  } else if (current < from) {
+    console.log('low');
+    return 'Low';
+  } else if (current > to) {
+    console.log('high');
+    return 'High';
+  } else {
+    return 'Normal';
+  }
+}
 
 export function VitalSignsCard(props) {
+  const {name, symbols, from, to, currnet} = props;
+  let color = ColorRange(from, to, currnet);
+  console.log(color);
   return (
     <Card>
       <View style={styles.container}>
-        <View style={[styles.Vital, { borderColor: "#FCCA4A" }]}>
-          <Text style={{ fontSize: 33, fontWeight: "bold", color: "#838487" }}>
-            50
+        <View style={[styles.Vital, {borderColor: color}]}>
+          <Text style={{fontSize: 33, fontWeight: 'bold', color: '#838487'}}>
+            {currnet}
           </Text>
         </View>
-        <Text style={styles.Sings}>&#8451;</Text>
+        <Text style={styles.Sings}>{symbols}</Text>
         <View style={styles.status}>
-          <Text style={[styles.textCenter, styles.text]}>Oxygen level</Text>
-          <Text style={[styles.textCenter, styles.text]}>Temperature</Text>
+          <Text style={[styles.textCenter, styles.text]}>{name}</Text>
+          <Text style={[styles.textCenter, styles.text, {color: color}]}>
+            {Status(from, to, currnet)}
+          </Text>
         </View>
       </View>
     </Card>
@@ -22,25 +56,25 @@ export function VitalSignsCard(props) {
 }
 
 export function BabyInfoCard(props) {
-  const { color, img, name, status, isSleep } = props;
+  const {color, img, name, status, isSleep, from, to} = props;
   const [statusColor, setStatusColor] = React.useState(color);
   return (
     <Card>
       <View style={styles.container}>
-        <View style={[styles.Vital, { borderColor: statusColor }]}>
+        <View style={[styles.Vital, {borderColor: statusColor}]}>
           {!!img && (
             <Image
               source={img}
-              style={{ width: 80, height: 80, borderRadius: 50 }}
+              style={{width: 80, height: 80, borderRadius: 50}}
             />
           )}
         </View>
         <View style={styles.info}>
           <Text style={[styles.smallText]}>{name}</Text>
           {!!status && (
-            <View style={{ display: "flex", flexDirection: "row" }}>
+            <View style={{display: 'flex', flexDirection: 'row'}}>
               <Text style={[styles.smallText]}>Status:</Text>
-              <Text style={[styles.smallText, { color: statusColor }]}>
+              <Text style={[styles.smallText, {color: statusColor}]}>
                 {status}
               </Text>
             </View>
@@ -56,45 +90,45 @@ export function BabyInfoCard(props) {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    width: "100%",
+    flexDirection: 'row',
+    width: '100%',
   },
   Vital: {
     width: 90,
     height: 90,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderWidth: 9,
     borderRadius: 50,
   },
   Sings: {
     fontSize: 43,
-    fontWeight: "bold",
-    color: "#838487",
-    alignSelf: "center",
+    fontWeight: 'bold',
+    color: '#838487',
+    alignSelf: 'center',
     paddingLeft: 5,
   },
   status: {
     width: 160,
     padding: 5,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   textCenter: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   text: {
     fontSize: 20,
-    fontWeight: "bold",
-    color: "#838487",
+    fontWeight: 'bold',
+    color: '#838487',
   },
   smallText: {
     fontSize: 17,
-    fontWeight: "bold",
-    color: "#838487",
+    fontWeight: 'bold',
+    color: '#838487',
   },
   info: {
     padding: 5,
     paddingLeft: 10,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
 });
--- screens/Placeholder.js
+++ screens/Placeholder.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { Text, View } from "react-native";
-import Card from "../components/Card";
-import {VitalSignsCard ,BabyInfoCard} from "../components/VitalSignsCard";
+import React from 'react';
+import {Text, View} from 'react-native';
+import Card from '../components/Card';
+import Button from '../components/Button';
+import {VitalSignsCard, BabyInfoCard} from '../components/VitalSignsCard';
 
 export default function Placeholder() {
+  const [currnet, setCurrnet] = React.useState(0);
+  console.log(currnet);
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <BabyInfoCard  color="#fff" />
-        <VitalSignsCard  color="#fff" />
-
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <BabyInfoCard color="#fff" />
+      <VitalSignsCard
+        symbols={'c'}
+        name="Oxygen level"
+        from={10}
+        to={20}
+        currnet={currnet}
+        color="#fff"
+      />
+      <View style={{paddingVertical: 10, paddingTop: 20}}>
+        <Button title="currnet+5" onPress={() => setCurrnet(currnet + 5)} />
+      </View>
+      <View style={{paddingVertical: 10}}>
+        <Button title="currnet-5" onPress={() => setCurrnet(currnet - 5)} />
+      </View>
     </View>
   );
 }
