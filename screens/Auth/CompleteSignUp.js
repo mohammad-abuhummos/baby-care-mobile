8e24917b494fe12afe99037258ccebc56b1ffc24
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-12-15 18:48:30 +0200

    add back button

--- screens/Auth/CompleteSignUp.js
+++ screens/Auth/CompleteSignUp.js
@@ -1,79 +1,112 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { useFonts } from "expo-font";
-import Button from "../../components/Button";
-import AppInput from "../../components/AppInput";
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Button from '../../components/Button';
+import AppInput from '../../components/AppInput';
 
-export default function CompleteSignUpScreen() {
-  let [fontsLoaded] = useFonts({
-    Pacifico: require("../../assets/Pacifico.ttf"),
-  });
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [retypePassword, setRetypePassword] = React.useState("");
+export default function CompleteSignUpScreen({navigation}) {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [retypePassword, setRetypePassword] = React.useState('');
   const input = {
     email: email,
     password: password,
     retypePassword: retypePassword,
   };
+  const validate = (email, password) => {
+    let user_account = new UserAccount(
+      email,
+      password,
+      retypePassword,
+    );
+    if (sign_in_info.isValid()) {
+      Alert.alert('valde');
+    } else {
+      console.log(sign_in_info.errors());
+      displayError('Invalid Information', sign_in_info.errors().join(', '));
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={{ paddingTop: 20 }}>
-        {!!fontsLoaded && <Text style={styles.HedaerPink}>Sign Up</Text>}
+    <SafeAreaView style={styles.container}>
+    <ScrollView>
+      <View style={{position: 'absolute', top: 30, left: 20}}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image
+            source={require('../../assets/back-icon.png')}
+            style={{
+              width: 50,
+              height: 50,
+              resizeMode: 'center',
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={{paddingTop: 20,alignItems:"center"}}>
+        <Text style={styles.HedaerPink}>Sign Up</Text>
       </View>
       <View style={styles.InnerContainer}>
-        <View style={{ paddingTop: 10 }}>
+        <View style={{paddingTop: 10}}>
           <Image
-            source={require("../../assets/logo.png")}
+            source={require('../../assets/logo.png')}
             style={{
               width: 180,
               height: 180,
-              resizeMode: "center",
+              resizeMode: 'center',
               aspectRatio: 1 / 2,
             }}
           />
         </View>
-        <View style={{ paddingTop: 30 }}>
+        <View style={{paddingTop: 30}}>
           <AppInput label="Email" onChangeText={(text) => setEmail(text)} />
         </View>
-        <View style={{ paddingTop: 30 }}>
+        <View style={{paddingTop: 30}}>
           <AppInput
             label="Password"
             onChangeText={(text) => setPassword(text)}
             secureTextEntry={true}
           />
         </View>
-        <View style={{ paddingTop: 30 }}>
+        <View style={{paddingTop: 30}}>
           <AppInput
             label="Retype password"
             onChangeText={(text) => setRetypePassword(text)}
             secureTextEntry={true}
           />
         </View>
-        <View style={{ paddingTop: 30 }}>
+        <View style={{paddingTop: 30}}>
           <Button title="Finsh" />
         </View>
       </View>
-    </View>
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fafafa",
-    alignItems: "center",
+    backgroundColor: '#fafafa',
+    alignItems: 'center',
   },
   HedaerPink: {
-    color: "#EE979F",
-    fontFamily: "Pacifico",
+    color: '#EE979F',
+    fontFamily: 'Pacifico',
     fontSize: 36,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     padding: 10,
+    
   },
   InnerContainer: {
     paddingHorizontal: 20,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- screens/Auth/SignUp.js
+++ screens/Auth/SignUp.js
@@ -1,75 +1,106 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import Button from "../../components/Button";
-import AppInput from "../../components/AppInput";
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import Button from '../../components/Button';
+import AppInput from '../../components/AppInput';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export default function SignUpScreen() {
-  const [firstname, setFirstname] = React.useState("");
-  const [lastname, setLastname] = React.useState("");
-  const [phone, setPhone] = React.useState("");
+export default function SignUpScreen({navigation}) {
+  const [firstname, setFirstname] = React.useState('');
+  const [lastname, setLastname] = React.useState('');
+  const [phone, setPhone] = React.useState('');
   const input = {
     firstname: firstname,
     lastname: lastname,
     phone: phone,
   };
   return (
-    <View style={styles.container}>
-      <View style={{ paddingTop: 20 }}>
-         <Text style={styles.HedaerPink}>Sign Up</Text>
-      </View>
-      <View style={styles.InnerContainer}>
-        <View style={{ paddingTop: 10 }}>
-          <Image
-            source={require("../../assets/logo.png")}
-            style={{
-              width: 180,
-              height: 180,
-              resizeMode: "center",
-              aspectRatio: 1 / 2,
-            }}
-          />
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={{position: 'absolute', top: 30, left: 20}}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image
+              source={require('../../assets/back-icon.png')}
+              style={{
+                width: 50,
+                height: 50,
+                resizeMode: 'center',
+              }}
+            />
+          </TouchableOpacity>
         </View>
-        <View style={{ paddingTop: 30 }}>
-          <AppInput
-            label="First name"
-            onChangeText={(text) => setFirstname(text)}
-          />
+        <View style={{paddingTop: 20, alignItems: 'center'}}>
+          <Text style={styles.HedaerPink}>Sign Up</Text>
         </View>
-        <View style={{ paddingTop: 30 }}>
-          <AppInput
-            label="Last name"
-            onChangeText={(text) => setLastname(text)}
-          />
+        <View style={styles.InnerContainer}>
+          <View style={{paddingTop: 10}}>
+            <Image
+              source={require('../../assets/logo.png')}
+              style={{
+                width: 180,
+                height: 180,
+                resizeMode: 'center',
+                aspectRatio: 1 / 2,
+              }}
+            />
+          </View>
+          <View style={{paddingTop: 30}}>
+            <AppInput
+              label="First name"
+              onChangeText={(text) => setFirstname(text)}
+            />
+          </View>
+          <View style={{paddingTop: 30}}>
+            <AppInput
+              label="Last name"
+              onChangeText={(text) => setLastname(text)}
+            />
+          </View>
+          <View style={{paddingTop: 30}}>
+            <AppInput
+              label="Phone number"
+              onChangeText={(text) => setPhone(text)}
+            />
+          </View>
+          <View style={{paddingTop: 30}}>
+            <Button
+              title="Next"
+              onPress={() => {
+                navigation.navigate('CompleteSignUp', {
+                  firstname: firstname,
+                  lastname: lastname,
+                  phone: phone,
+                });
+              }}
+            />
+          </View>
         </View>
-        <View style={{ paddingTop: 30 }}>
-          <AppInput
-            label="Phone number"
-            onChangeText={(text) => setPhone(text)}
-          />
-        </View>
-        <View style={{ paddingTop: 30 }}>
-          <Button title="Next" />
-        </View>
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fafafa",
-    alignItems: "center",
+    backgroundColor: '#fafafa',
+    alignItems: 'center',
   },
   HedaerPink: {
-    color: "#EE979F",
+    color: '#EE979F',
     fontSize: 36,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     padding: 10,
   },
   InnerContainer: {
     paddingHorizontal: 20,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
