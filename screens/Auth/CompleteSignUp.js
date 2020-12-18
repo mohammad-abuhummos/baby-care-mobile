ea5e950e455d869e16ea49b9b1ef314024f0a390
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-12-18 20:29:40 +0200

    sende user info to databse

--- screens/Auth/CompleteSignUp.js
+++ screens/Auth/CompleteSignUp.js
@@ -10,83 +10,95 @@ import {
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import Button from '../../components/Button';
 import AppInput from '../../components/AppInput';
-
+import {UserContext} from '../../App';
+import {displayError} from '../../models/helpers';
+import CompleteSignUp from '../../models/CompleteSignUp';
+import database from '@react-native-firebase/database';
 export default function CompleteSignUpScreen({navigation}) {
+  const id = React.useContext(UserContext);
+  console.log('authContext', id);
+  const [loading, setLoading] = React.useState(false);
   const [firstname, setFirstname] = React.useState('');
   const [lastname, setLastname] = React.useState('');
   const [phone, setPhone] = React.useState('');
-  const input = {
-    firstname: firstname,
-    lastname: lastname,
-    phone: phone,
-  };
-  const validate = (email, password) => {
-    let user_account = new UserAccount(
-      email,
-      password,
-      retypePassword,
-    );
-    if (sign_in_info.isValid()) {
-      Alert.alert('valde');
+  const handleInfo = (firstname, lastname, phone) => {
+    let User_info = new CompleteSignUp(firstname, lastname, phone);
+    if (User_info.isValid()) {
+      const info = {
+        Firstname: firstname,
+        Lastname: lastname,
+        Phone: phone,
+      };
+      database()
+        .ref(`/users/${id}`)
+        .set({info})
+        .then(() => console.log('Data set.'));
     } else {
-      console.log(sign_in_info.errors());
-      displayError('Invalid Information', sign_in_info.errors().join(', '));
+      setLoading(false);
+      console.log(User_info.errors());
+      displayError('Invalid Information', User_info.errors().join(', '));
     }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-    <ScrollView>
-      <View style={{position: 'absolute', top: 30, left: 20}}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Image
-            source={require('../../assets/back-icon.png')}
-            style={{
-              width: 50,
-              height: 50,
-              resizeMode: 'center',
-            }}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={{paddingTop: 20,alignItems:"center"}}>
-        <Text style={styles.HedaerPink}>Sign Up</Text>
-      </View>
-      <View style={styles.InnerContainer}>
-        <View style={{paddingTop: 10}}>
-          <Image
-            source={require('../../assets/logo.png')}
-            style={{
-              width: 180,
-              height: 180,
-              resizeMode: 'center',
-              aspectRatio: 1 / 2,
-            }}
-          />
+      <ScrollView>
+        <View style={{position: 'absolute', top: 30, left: 20}}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image
+              source={require('../../assets/back-icon.png')}
+              style={{
+                width: 50,
+                height: 50,
+                resizeMode: 'center',
+              }}
+            />
+          </TouchableOpacity>
         </View>
-        <View style={{paddingTop: 30}}>
+        <View style={{paddingTop: 20, alignItems: 'center'}}>
+          <Text style={styles.HedaerPink}>Complete Profile</Text>
+        </View>
+        <View style={styles.InnerContainer}>
+          <View style={{paddingTop: 10}}>
+            <Image
+              source={require('../../assets/logo.png')}
+              style={{
+                width: 180,
+                height: 180,
+                resizeMode: 'center',
+                aspectRatio: 1 / 2,
+              }}
+            />
+          </View>
+          <View style={{paddingTop: 30}}>
             <AppInput
               label="First name"
+              autoCapitalize="words"
               onChangeText={(text) => setFirstname(text)}
             />
           </View>
           <View style={{paddingTop: 30}}>
             <AppInput
               label="Last name"
+              autoCapitalize="words"
               onChangeText={(text) => setLastname(text)}
             />
           </View>
           <View style={{paddingTop: 30}}>
             <AppInput
               label="Phone number"
+              keyboardType="phone-pad"
               onChangeText={(text) => setPhone(text)}
             />
           </View>
-        <View style={{paddingTop: 30}}>
-          <Button title="Finsh" />
+          <View style={{paddingTop: 30}}>
+            <Button
+              title="Finsh"
+              onPress={() => handleInfo(firstname, lastname, phone)}
+            />
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -103,7 +115,6 @@ const styles = StyleSheet.create({
     fontSize: 36,
     fontWeight: 'normal',
     padding: 10,
-    
   },
   InnerContainer: {
     paddingHorizontal: 20,
